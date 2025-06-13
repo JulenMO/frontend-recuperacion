7473c6371a0ac2fb125fb9148479c32303b16c47
julenmo <julen.medina.oses@gmail.com>
2025-06-13 13:39:01 +0200

    feat: Create pizza list component

--- src/app/components/pizza-list/pizza-list.component.ts
+++ src/app/components/pizza-list/pizza-list.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pizza, PizzaService } from '../../services/pizza.service';
+import { PizzaCardComponent } from "../pizza-card/pizza-card.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pizza-list',
-  imports: [],
   templateUrl: './pizza-list.component.html',
-  styleUrl: './pizza-list.component.scss'
+  styleUrls: ['./pizza-list.component.scss'],
+  imports: [PizzaCardComponent, CommonModule]
 })
-export class PizzaListComponent {
+export class PizzaListComponent implements OnInit {
+  pizzas: Pizza[] = [];
+  error = '';
 
+  constructor(private pizzaService: PizzaService) { }
+
+  ngOnInit(): void {
+    this.loadPizzas();
+  }
+
+  loadPizzas(): void {
+    this.pizzaService.getPizzas().subscribe({
+      next: data => {
+        this.pizzas = data;
+      },
+      error: err => {
+        this.error = 'Error cargando pizzas.';
+      }
+    });
+  }
+
+  onAddToCart(event: { pizza: Pizza; quantity: number }) {
+    // Por ahora solo consola, luego enviaremos a carrito/servicio
+    console.log('Añadido al carrito:', event);
+  }
 }
